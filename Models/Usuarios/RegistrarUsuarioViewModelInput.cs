4b9bfb7ce9865580da01e9946da0c7601f4c3828
Felipe Fadil Caetano <felipefadil@gmail.com>
2021-06-28 22:08:08 -0300

    Projeto finalizado

---
+++ Models/Usuarios/RegistrarUsuarioViewModelInput.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace curso.web.mvc.Models.Usuarios
+{
+    public class RegistrarUsuarioViewModelInput
+    {
+        [Required(ErrorMessage = "O login é obrigatório")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "O email é inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        public string Senha { get; set; }
+    }
+}
